e2e8049f09e724eccc0590a48128d2ac7b655738
JOEUN PARK <mrnglory@mail.hongik.ac.kr>
2021-02-22 15:57:59 +0900

    datatype

---
+++ Node-JS-Basic-Syntax/datatype.js
@@ -0,0 +1,18 @@
+var length = 16;
+var lastName = "Johnson"
+var x = {firstName: "John", lastName: "Doe"};
+
+var x = 16 + "Volvo";
+console.log(x);
+
+var y = "16" + "Volvo";
+console.log(y);
+
+var z = 16 + "Volvo";
+console.log(z);
+
+var a = "Volvo" + 16;
+console.log(a);
+
+var b = 16 + 4 + "Volvo";
+console.log(b);
