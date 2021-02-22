dec4dfc302b7074c4376abb476f24cdbb244cd08
JOEUN PARK <mrnglory@mail.hongik.ac.kr>
2021-02-22 16:13:10 +0900

    operator

---
+++ Node-JS-Basic-Syntax/operator.js
@@ -0,0 +1,15 @@
+<script>
+    var x = 5;
+    var y = 2;
+    var z = x + y;
+    var a = x - y;
+    var b = x * y;
+    var c = x / y;
+
+    console.log("x: " + x);
+    console.log("y: " + y);
+    console.log("z: " + z);
+    console.log("a: " + a);
+    console.log("b: " + b);
+    console.log("c: " + c);
+</script>
