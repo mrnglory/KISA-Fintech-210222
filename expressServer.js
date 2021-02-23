a5d49e91bb816ba9fba00652a70aba36e2da129d
JOEUN PARK <mrnglory@mail.hongik.ac.kr>
2021-02-23 16:36:54 +0900

    express server with database

--- expressServer.js
+++ expressServer.js
@@ -1,12 +1,30 @@
 const express = require('express')
 const app = express()
 
+
+////////////
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'p446851',
+    database : 'kisa-fintech210222',
+    port     : '3307'
+});
+connection.connect();
+// connection.end();
+///////////
+
+
 app.get('/', function (req, res) {
     res.send('Hello World')
 })
 
 app.get('/user', function (req, res) {
-    res.send('사용자 페이지입니다.')
+    connection.query('SELECT * FROM user;', function(error, results, fields) {
+        console.log(results);
+        res.send(results);
+    });
 })
 
 app.listen(3000)
