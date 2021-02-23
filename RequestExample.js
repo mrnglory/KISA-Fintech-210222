1a0e94c44eec40b966b2e23a143e176e25249abd
JOEUN PARK <mrnglory@mail.hongik.ac.kr>
2021-02-23 14:17:39 +0900

    xml to javascript

--- RequestExample.js
+++ RequestExample.js
@@ -1,4 +1,10 @@
 const request = require('request');
+var parseString = require('xml2js').parseString;
+
 request('http://www.weather.go.kr/weather/forecast/mid-term-rss3.jsp?stnld=109', function(error, response, body) {
     console.log('body:',body);
+    var xml = body;
+    parseString(xml, function(err, result) {
+        console.dir(result);
+    });
 });
