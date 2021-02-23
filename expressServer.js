7418cc69cf8683cb98aabba98c41f221ee707cdb
JOEUN PARK <mrnglory@mail.hongik.ac.kr>
2021-02-23 16:43:50 +0900

    adding ejs module

--- expressServer.js
+++ expressServer.js
@@ -1,5 +1,22 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+
+// 뷰 파일이 있는 디렉토리를 설정
+app.set('views', __dirname + '/views');
+// 뷰 엔진으로 ejs 사용
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+    res.send('Hello World');
+})
+
+app.get('/user', function (req, res) {
+    connection.query('SELECT * FROM user;', function(error, results, fields) {
+        console.log(results);
+        res.send(results);
+    });
+})
+
 
 
 ////////////
@@ -16,15 +33,5 @@ connection.connect();
 ///////////
 
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
-})
 
-app.get('/user', function (req, res) {
-    connection.query('SELECT * FROM user;', function(error, results, fields) {
-        console.log(results);
-        res.send(results);
-    });
-})
-
-app.listen(3000)
+app.listen(3000);
