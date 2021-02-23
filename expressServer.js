a67004dc1d48e9adcaf4aafadee0823af860d615
JOEUN PARK <mrnglory@mail.hongik.ac.kr>
2021-02-23 16:46:06 +0900

    webpage made by ejs

--- expressServer.js
+++ expressServer.js
@@ -10,6 +10,10 @@ app.get('/', function (req, res) {
     res.send('Hello World');
 })
 
+app.get('/ejs', function (req, res) {
+    res.render('ejsTest');
+})
+
 app.get('/user', function (req, res) {
     connection.query('SELECT * FROM user;', function(error, results, fields) {
         console.log(results);
