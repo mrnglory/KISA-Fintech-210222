8e751343dd5e98eefa11fba918e67dad3969fcde
JOEUN PARK <mrnglory@mail.hongik.ac.kr>
2021-02-24 16:28:11 +0900

    로그인 기능 완성 서비스 토큰 생성 완성

--- expressServer.js
+++ expressServer.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const request = require('request');
+var jwt = require('jsonwebtoken');
 
 // json 타입의 데이터 전송을 허용
 app.use(express.json());
@@ -76,6 +77,52 @@ app.post('/signup', function(req, res) {
             res.json(1);
         }
     });
+
+})
+
+app.post('/login', function(req, res){
+    var userEmail = req.body.userEmail;
+    var userPassword = req.body.userPassword;
+    console.log(userEmail, userPassword)
+    var sql = "SELECT * FROM user WHERE email = ?";
+    connection.query(sql, [userEmail], function(err, result){
+        if(err){
+            console.error(err);
+            res.json(0);
+            throw err;
+        }
+        else {
+            if(result.length == 0){
+                res.json(3)
+            }
+            else {
+                var dbPassword = result[0].password;
+                if(dbPassword == userPassword){
+                    var tokenKey = "f@i#n%tne#ckfhlafkd0102test!@#%"
+                    jwt.sign(
+                        {
+                            userId : result[0].id,
+                            userEmail : result[0].email
+                        },
+                        tokenKey,
+                        {
+                            expiresIn : '10d',
+                            issuer : 'fintech.admin',
+                            subject : 'user.login.info'
+                        },
+                        function(err, token){
+                            console.log('로그인 성공', token)
+                            res.json(token)
+                        }
+                    )
+                }
+                else {
+
+                    res.json(2);
+                }
+            }
+        }
+    })
 
 })
 
