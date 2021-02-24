e50d764d776af59b286308833046ac6a46ecb4b5
JOEUN PARK <mrnglory@mail.hongik.ac.kr>
2021-02-24 10:34:34 +0900

    사용자 요청 처리 라우터 추가

--- expressServer.js
+++ expressServer.js
@@ -14,6 +14,10 @@ app.get('/ejs', function (req, res) {
     res.render('ejsTest');
 })
 
+app.post('/userData', function(req, res) {
+    console.log("사용자의 요청이 발생했습니다.");
+})
+
 app.get('/user', function (req, res) {
     connection.query('SELECT * FROM user;', function(error, results, fields) {
         console.log(results);
