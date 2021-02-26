642e479f1decf1895b14d9d18b8969268ce21ffb
JOEUN PARK <mrnglory@mail.hongik.ac.kr>
2021-02-26 15:18:13 +0900

    table scraping

--- scraping/hello.py
+++ scraping/hello.py
@@ -1,1 +1,8 @@
-print("hello python")
+from selenium import webdriver
+driver = webdriver.Chrome('./chromedriver')
+driver.implicitly_wait(3)
+driver.get('https://news.naver.com/main/read.nhn?mode=LSD6mid=shm&sid=105&oid=138&aid=0002099390')
+title = driver.find_element_by_id('article')
+body = driver.find_element_by_id('articleBodyContents')
+print(title.text)
+print(body.text)
